e35636e05d77faca475f21c348280b6fa8d8ea88
Jakub Pilarek <pilarek.jakub@gmail.com>
2022-04-03 15:21:34 +0200

    Komentarz

---
+++ src/main/java/pl/sda/arppl/Main.java
@@ -0,0 +1,8 @@
+package pl.sda.arppl;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Gotowe!);
+
+    }
+}
---
+++ src/main/java/pl/sda/arppl/pracawzespole.java
@@ -0,0 +1,4 @@
+package pl.sda.arppl;
+
+public class pracawzespole {
+}
