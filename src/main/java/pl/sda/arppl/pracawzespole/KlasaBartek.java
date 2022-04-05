1b90ccb5f01d5be8c71faf499dc3f024ddb1b832
bKlamann92 <bartosz.klamann@gmail.com>
2022-04-05 17:36:51 +0200

    Dodane zadanie 1

--- src/main/java/pl/sda/arppl/pracawzespole/KlasaBartek.java
+++ src/main/java/pl/sda/arppl/pracawzespole/KlasaBartek.java
@@ -1,2 +1,28 @@
-package pl.sda.arppl.pracawzespole;public class KlasaBartek {
+package pl.sda.arppl.pracawzespole;
+
+import java.util.Scanner;
+
+public class KlasaBartek {
+
+    public static void main(String[] args) {
+        System.out.println("Hej! Podaj swoje imię");
+        Scanner scanner = new Scanner(System.in);
+        String imie  = scanner.next();
+
+        System.out.println("Podaj swoje nazwisko");
+        String nazwisko = scanner.next();
+
+        System.out.println("Podaj dowolną liczbę zmiennoprzecinkową");
+        double liczba1 = scanner.nextDouble();
+
+        System.out.println("Podaj kolejną liczbę zmiennoprzecinkową");
+        double liczba2 = scanner.nextDouble();
+
+        double suma = liczba1 + liczba2;
+        double roznica = liczba1 - liczba2;
+
+        System.out.println("Hej " + imie + " " + nazwisko + "! Czy wiesz, że suma podanych liczb to " + suma + ", a różnica to " + roznica );
+
+    }
 }
+
